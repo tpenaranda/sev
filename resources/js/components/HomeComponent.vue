fa90f93fbe6cb7f0db45ec3afb9c931048e8ab64
Tate Peñaranda <tate@blockinit.io>
2019-04-08 14:49:42 -0300

    Initial commit. Added empty Laravel project and VueJS.

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jumbotron vh-100 d-flex align-items-center m-0">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Streamer Event Viewer</div>
+                        <div class="card-body">
+                            [Body placeholder]
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        }
+    }
+</script>
